1b4bb5f1d70789274e0103e9c19e8dbc26d7174b
Colonial1 <112031206+Colonial1@users.noreply.github.com>
2022-08-30 09:42:47 -0600

    Create Variables.cs

---
+++ Variables.cs
@@ -0,0 +1,39 @@
+using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string name = "A";
+      Console.WriteLine(name);  
+     }
+  }
+}
+using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string name = "B";
+      Console.WriteLine(name);  
+     }
+  }
+}
+using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+       
+      
+    }
+  }
+}

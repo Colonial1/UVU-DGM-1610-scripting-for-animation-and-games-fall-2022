347563575cf8a80d5a027b0c13b144e1bdcacaa7
Colonial1 <112031206+Colonial1@users.noreply.github.com>
2022-10-24 18:11:44 -0600

    Update CollectObject.cs

--- CollectObject.cs
+++ CollectObject.cs
@@ -4,21 +4,19 @@ using UnityEngine;
 
 public class CollectObject : MonoBehaviour
 {
-    private Object thisObject;
+   public class DetectCollision : MonoBehaviour
+{
+    
+    private ScoreManager scoreManager; // A variable to hold the reference to the scoremanager
+    public int scoreToGive;
+   
 
-    private void Awake()
+    // Start is called before the first frame update
+    void Start()
     {
-        thisObject = GetComponent<Object>();
+        scoreManager = GameObject.Find("ScoreManager").GetComponent<ScoreManager>(); // Referance scoremanager
+       
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-
-        if (collision.CompareTag("PlayerCollider"))
-        {
-           PlayerPrefs.SetInt(thisObject.ID, PlayerPrefs.GetInt(thisObject.ID) + 1);
-            Destroy(gameObject);
-        }
-    }
 }
-
+}

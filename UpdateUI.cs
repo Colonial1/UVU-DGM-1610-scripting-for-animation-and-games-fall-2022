1f0e313c606b3b3a02df0399135624ccd9f7a9ec
Colonial1 <112031206+Colonial1@users.noreply.github.com>
2022-10-24 18:12:41 -0600

    Add files via upload

---
+++ UpdateUI.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ScoreManager : MonoBehaviour
+
+{
+    public int score; // Store score value
+
+    public TextMeshProUGUI scoreText; // Referance visual text UI element to change
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    //This function rewards the player 
+    public void IncreaseScore(int amount)
+    {
+        score += amount; //add amount to the score 
+        UpdateScoreText(); // Update the score UI text
+    }
+    //This function penalizes the player 
+    public void DecreaseScore(int amount)
+    {
+        score -= amount; //Subtract amount to the score 
+        UpdateScoreText(); // Update the score UI text
+    }
+
+   public void UpdateScoreText()
+   {
+        scoreText.text = "Score: " + score;
+   }
+}

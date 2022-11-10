2eacabd499cf441b4589427f67e0c4818a6a2b00
Colonial1 <112031206+Colonial1@users.noreply.github.com>
2022-11-09 22:21:38 -0700

    Add files via upload

---
+++ EnemyAttack.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour
+{
+    private PlayerHealth playerHealth;
+    public int damage = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerHealth = GameObject.Find("Player").GetComponent<PlayerHealth>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        
+         playerHealth.TakeDamage(damage);
+         Debug.Log("Player Takes" + damage + " points of damage");
+           
+
+    }
+}

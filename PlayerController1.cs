28131844c9e2396ef17965efed40e00a2c175631
Colonial1 <112031206+Colonial1@users.noreply.github.com>
2022-12-04 14:00:58 -0700

    Update PlayerController1.cs

--- PlayerController1.cs
+++ PlayerController1.cs
@@ -2,32 +2,68 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController1 : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     [Header("Player Movement")]
 
     public float moveSpeed;
     public float jumpForce;
+    
+    public int curHp;
+    public int maxHp;
+
     [Header("Camera")]
     public float lookSensitivity;
     public float maxLookX;
     public float minLookX;
     private float rotX;
+    
     private Camera camera;
     private Rigidbody rb;
+    private Weapon weapon;
     
     void Awake()
     {
-        camera = Camera.main;
-        rb = GetComponent<Rigidbody>();
+      //weapon = GetComponent<Weapon>();
     }
    
     // Start is called before the first frame update
     void Start()
     {
-        
+        camera = Camera.main;
+        rb = GetComponent<Rigidbody>();
     }
 
+
+    public void TakeDamage(int damage)
+    {
+        curHp -= damage;
+        
+        if(curHp <= 0)
+            Die();
+        
+        GameManager.instance.UpdateHealthBar(curHp, maxHp);
+    }
+    
+    void Die()
+    {
+        GameManager.instance.LoseGame();
+        Debug.Log("Player has died! Game Over!");
+    }
+
+     public void GiveHealth(int amountToGive)
+     {
+        //curHp = Mathf.Clamp(curHp + amountToGive, 0, maxHp);
+       // GameUI.instance.UpdateHealthBar(curHp, maxHp);
+         Debug.Log("Player has been healed!");
+     }
+
+     public void GiveAmmo(int amountToGive)
+     {
+        //weapon.curAmmo = Mathf.Clamp(weapon.curAmmo + amountToGive, 0, weapon.maxAmmo);
+       // GameUI.instance.UpdateAmmoText(weapon.curAmmo, weapon.maxAmmo);
+     }
+    
     // Update is called once per frame
     void Update()
     {
@@ -39,6 +75,7 @@ public class PlayerController1 : MonoBehaviour
         }
     }
     
+    
     void Move()
     {
         float x = Input.GetAxis("Horizontal") * moveSpeed;
@@ -46,7 +83,7 @@ public class PlayerController1 : MonoBehaviour
 
         Vector3 dir =transform.right * x + transform.forward *z;
         dir.y = rb.velocity.y;
-
+        rb.velocity = dir;
         //rb.velocity = new Vector3(x, rb.velocity.y, z);
     }
 
@@ -69,4 +106,5 @@ public class PlayerController1 : MonoBehaviour
             rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
         }
     }
+  
 }
